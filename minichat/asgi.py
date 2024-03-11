e9a145b1ac3c1487a043899bc5973ac8bf208b18
Jay <odionye.jude@outlook.com>
2024-03-11 13:08:05 +0100

    jwt auth middleware

--- minichat/asgi.py
+++ minichat/asgi.py
@@ -6,13 +6,14 @@ from django.core.asgi import get_asgi_application
 from django.urls import path
 
 from chat import routing
-from chat.consumers import ChatConsumer
+
+from .middleware import JWTAuthMiddleware
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "minichat.settings")
 
 application = ProtocolTypeRouter(
     {
         "http": get_asgi_application(),
-        "websocket": AllowedHostsOriginValidator(URLRouter(routing.websocket_urlpatterns)),
+        "websocket": AllowedHostsOriginValidator(JWTAuthMiddleware(URLRouter(routing.websocket_urlpatterns))),
     }
 )
