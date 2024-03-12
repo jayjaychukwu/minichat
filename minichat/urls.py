8279ddd67518edbf5b473cfcad0f8f0323aae013
Jay <odionye.jude@outlook.com>
2024-03-12 12:53:24 +0100

    imports clean up

--- minichat/urls.py
+++ minichat/urls.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
 from django.urls import include, path
 
-from chat.consumers import ChatConsumer
-
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("accounts.urls")),
